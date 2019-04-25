1d16a521732fdb7268cca8cef52ccaf75d7e50cf
Gediminas <gediminas.zubovicius@gmail.com>
2019-04-25 15:59:39 +0100

    First actual commit

---
+++ lib/mister_pasha_api.rb
@@ -0,0 +1,6 @@
+require "mister_pasha_api/version"
+
+module MisterPashaApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mister_pasha_api/version.rb
@@ -0,0 +1,3 @@
+module MisterPashaApi
+  VERSION = "0.1.0"
+end
